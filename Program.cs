b5c969b4d871b6d94b0e837e8222970e7170d34a
“Yashwanth142” <yashwanthvishnu14@gmail.com>
2023-01-13 15:00:54 +0530

    Welcome

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace BinarySearchTree
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            BinaryTree binaryTree = new BinaryTree();
+
+            binaryTree.Add(70);
+            binaryTree.Add(30);
+            binaryTree.Add(25);
+            binaryTree.Add(35);
+            binaryTree.Add(80);
+            binaryTree.Add(75);
+
+            binaryTree.TraversePreorder(binaryTree.Root);
+        }
+    }
+}
